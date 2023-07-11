a54fc548d99c8b849de8d36aca50c3d42b632223
fadiabji <fadi.abji@htomail.com>
2023-07-11 16:05:01 +0200

    Add project files.

---
+++ FirstBlazorApp/Client/Models/Todo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace FirstBlazorApp.Client.Models
+{
+    public class Todo
+    {
+        public string Title { get; set; }
+        public bool Isompleted { get; set; }
+    }
+}
